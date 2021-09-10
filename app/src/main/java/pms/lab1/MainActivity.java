124d73010ced1616472c04f7f75ec9ddd8b12406
alesyademidkevich <alesya.demidkevic@mail.ru>
2021-09-10 23:20:35 +0300

    Initial commit

---
+++ app/src/main/java/pms/lab1/MainActivity.java
@@ -0,0 +1,15 @@
+package pms.lab1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    //TODO add new method
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

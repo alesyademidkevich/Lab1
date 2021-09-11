75fe46214aa131f865d73f3a666d7129d1edcf0a
alesyademidkevich <alesya.demidkevic@mail.ru>
2021-09-11 08:45:59 +0300

    new

--- app/src/main/java/pms/lab1/MainActivity.java
+++ app/src/main/java/pms/lab1/MainActivity.java
@@ -3,6 +3,10 @@ package pms.lab1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import pms.lab1.text.TextFunction;
 
 public class MainActivity extends AppCompatActivity {
     //TODO add new method
@@ -11,5 +15,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displayText();
+
+        for (int count = 0; count < 10; count++) {
+            Log.d("MainActivity", " count =" + count);
+        }
+    }
+
+    private void displayText() {
+        TextFunction tf = new TextFunction();
+        TextView nt = findViewById(R.id.newtest);
+        nt.setText(tf.getValue());
     }
 }
---
+++ app/src/main/java/pms/lab1/text/TextFunction.java
@@ -0,0 +1,8 @@
+package pms.lab1.text;
+
+public class TextFunction {
+    public String getValue(){
+        return "Hello from first project";
+    }
+
+}
